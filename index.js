7b4fbaa83ff7c078f5c89c63d4fc3fe9b4febcd3
Nunnyr <nunnyreyes@gmail.com>
2020-09-17 19:37:00 -0400

    Read the shopping list

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Practice your JS here!
+let itemOL = document.querySelector(".shopping.checkout")
+let itemForm = document.querySelector("form#new-item")
+
+//We want to display our seed data
+
+fetch("http://localhost:3000/shopping")
+    .then(res => res.json())
+    .then(shoppingArr => {
+        //we have access to our shopping array in our db json
+        //iterate through this array to help faciliate being added to the DOM
+        shoppingArr.forEach( itemObj => {
+            
+            turnItemToLI(itemObj)
+        })
+    })
+
+let turnItemToLI = (shopping) => {
+    
+    //1) create the outermost element
+    let itemLi = document.createElement("li")
+        itemLi.className = "item"
+    //2) fill the contents of that element
+    itemLi.innerHTML = `<p class="js-item">${shopping.item} - <span>${shopping.count}</span></p>
+        <button class="quantity">Quantity</button>
+        <button class="delete-button">x</button>
+        `
+    //3) Slap the outermost element on the DOM
+    itemOL.append(itemLi)
+   
+}
